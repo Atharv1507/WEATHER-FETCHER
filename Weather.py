2ebb8f93387f3b2f2458fbad2a64e4c28d335e83
Atharv Aggarwal <aggarwal.atharv15@gmail.com>
2025-02-27 19:33:10 +0530

    Update Weather.py
    formating response and making things cleaner

--- Weather.py
+++ Weather.py
@@ -2,17 +2,25 @@ from pprint import pprint
 from dotenv import *
 import requests
 import os
+
 load_dotenv()
 
+end=""
+print("GET CURRENT WEATHER REPORT")
+
 def CurrentWeather():
-    print("GET CURRENT WEATHER REPORT")
-    city=input("please enter a city name:")
+ try:
+    city=input("please enter a city name:")   
     req_url=f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
-
     weatherData=requests.get(req_url).json()
+    print(f"\nThe weather conditions are \nName:{weatherData["name"]}\nWind:{weatherData["wind"]}\nCurrent Temp:{weatherData["main"]["temp"]}")
 
+ except:
+    print("Check the entered field")
     #To easily read and see data in a json file
     #pprint(weatherData)
-    print(f"The weather conditions are \nName:{weatherData["name"]}\nWind:{weatherData["wind"]}\nCurrent Temp{weatherData["main"]["temp"]}")
 
-CurrentWeather()
+while end!="y":    
+    CurrentWeather()
+    end=input("\nwould you like to quit(y/n):")
+
